79c7ed83444ee07b733a893a8f54a1d2138edb8b
Rawan <rawnq.sb@gmail.com>
2023-03-20 23:09:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Polygon {
+    constructor(polySides) { this.polySides = polySides }
+    get countSides() { return this.polySides.length }
+    get perimeter() {
+        const perimeter = this.polySides.reduce((sideA, sideB) => { return sideA + sideB });
+        return perimeter;
+    }
+}
+class Triangle extends Polygon {
+    constructor(polySides) { super(polySides) }
+    get isValid() {
+        if(this.polySides[0] + this.polySides[1] > this.polySides[2] && this.polySides[0] + this.polySides[2] > this.polySides[1] && this.polySides[1] + this.polySides[2] > this.polySides[0]) {
+            return true;
+        } else { return false }
+    }
+}
+class Square extends Polygon {
+    constructor(polySides) { super(polySides) }
+    get isValid() {
+        if (this.polySides[0] === this.polySides[1] && this.polySides[0] === this.polySides[2] && this.polySides[0] === this.polySides[3]) {
+            return true;
+        } else { return false }
+    }
+    get area() {
+        const perimeter = this.perimeter;
+        const area = perimeter / 4;
+        return area * area;
+    }
+}
